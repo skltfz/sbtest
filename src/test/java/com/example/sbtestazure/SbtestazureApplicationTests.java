17c7e92aba834522f1f39c7fc1433f874a68b8ec
skltfz <skltfz@gmail.com>
2024-08-05 11:23:52 +0800

    first commit

---
+++ src/test/java/com/example/sbtestazure/SbtestazureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtestazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtestazureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
